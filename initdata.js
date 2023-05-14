25a4ee686d48a25d36721e889957de8d08d67974
Phuc <phuchoang2411@gmail.com>
2023-05-14 15:59:16 +0700

    Fix name

--- initdata.js
+++ initdata.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 
-const connectDB = require('./db/connect');
-const Product = require('./models/product');
+const connectDB = require('./utils/database');
+const Book = require('./models/Book');
 
 const jsonBooks = require('./data/books.json');
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGODB_URL);
-    await Product.deleteMany();
-    await Product.create(jsonBooks);
+    await Book.deleteMany();
+    await Book.create(jsonBooks);
     console.log('Success!!!');
     process.exit(0);
   } catch (error) {
